8f5085063083b73a0a2b3d031cd72b9afb743ce7
mariafromkorea <maria@bluetent.com>
2018-09-10 20:02:51 -0600

    pass 1

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: "Contacts",
+    showDetails: false,
+    name:" ",
+    email:" ",
+    phone:" ",
+    company:" ",
+    title: " ",
+    notes:" ",
+    contacts: [
+      {
+        contactID:01,
+        contactName: "Chester Tester",
+        contactEmail: "chestertester@gmail.com",
+        contactPhone:"410-555-5555",
+        contactCompany:"AppOmni",
+        contactTitle:"Chief Security Officer",
+        contactNotes:"Never picks up his phone but responds via email."
+      },
+      {
+        contactID:02,
+        contactName: "Lester Tester",
+        contactEmail: "lestertester@gmail.com",
+        contactPhone:"410-555-5551",
+        contactCompany:"AppOmni",
+        contactTitle:"Chief Technology Officer",
+        contactNotes:"Can be bribed with Chocolate"
+      }
+    ]
+  },
+  methods: {
+    addContact: function() {
+
+    },
+      updateDetails: function(contactName, contactEmail, contactPhone, contactCompany, contactTitle, contactNotes) {
+      this.showDetails = true
+      this.email = contactEmail,
+      this.phone = contactPhone,
+      this.name = contactName,
+      this.company = contactCompany,
+      this.title = contactTitle,
+      this.notes = contactNotes
+    },
+    hideDetails: function () {
+      this.showDetails = false
+    }
+  },
+  computed {
+    
+  }
+})
